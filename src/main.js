c08898a4743ecbb58418c6f86af3d5795d5c71c5
Levi Weiss <levi.weiss3@gmail.com>
2020-05-27 23:34:35 +0300

    starting with vue router

--- src/main.js
+++ src/main.js
@@ -1,14 +1,27 @@
 import Vue from 'vue'
 import App from './App.vue'
 import Store from './store'
+import VueRouter from 'vue-router'
 
 export const bus = new Vue()
 
+Vue.use(VueRouter)
 Vue.config.productionTip = false
+
+const routes = [
+  { path: '/coupons', alias: '/', component: Coupons },
+  { path: '/businneses', component: Businneses }
+]
+
+// Create the router instance and pass the `routes` option
+const router = new VueRouter({
+  routes
+})
 
 new Vue({
   render (createElement) {
     return createElement(App)
   },
+  router,
   store: Store
 }).$mount('#app')
