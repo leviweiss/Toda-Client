73417388414843d10405a22f32024b0cb565a46e
Levi Weiss <levi.weiss3@gmail.com>
2020-05-27 19:47:44 +0300

    stable first

--- src/main.js
+++ src/main.js
@@ -2,9 +2,6 @@ import Vue from 'vue'
 import App from './App.vue'
 import Store from './store'
 
-import '@fortawesome/fontawesome-free/css/all.css'
-import '@fortawesome/fontawesome-free/js/all.js'
-
 export const bus = new Vue()
 
 Vue.config.productionTip = false
