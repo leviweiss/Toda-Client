f7b9c0e314fa26fde3238283e488f5cc32dcc664
Levi Weiss <levi.weiss3@gmail.com>
2020-05-28 15:34:18 +0300

    done with tab navigation

--- src/components/AppNav.vue
+++ src/components/AppNav.vue
@@ -1,59 +1,138 @@
 <template>
   <nav id="app-nav" class="app-nav-container">
     <img src="../assets/logo.png" class="app-nav-container__logo" @click="goToHome"/>
-    <router-link to="about-us" class="app-nav-container__about-us">
-      About us
-    </router-link>
-    <router-link to="coupons" class="app-nav-container__coupons">
-      Coupons
-    </router-link>
-    <router-link to="businesses" class="app-nav-container__businesses">
-      Businesses
-    </router-link>
+    <div class="app-nav-container__links">
+      <router-link to="home" class="app-nav-container__links__home">
+        Home
+      </router-link>
+      <router-link to="coupons" class="app-nav-container__links__coupons">
+        Coupons
+      </router-link>
+      <router-link to="businesses" class="app-nav-container__links__businesses">
+        Businesses
+      </router-link>
+      <router-link to="about-us" class="app-nav-container__links__about-us">
+        About-Us
+      </router-link>
+    </div>
   </nav>
 </template>
 
 <script>
+import { mapState, mapActions } from 'vuex';
+
 export default {
   name: 'AppNav',
+  computed: {
+    ...mapState({
+      activeTab: (state) => state.activeTab,
+    }),
+  },
   methods: {
     goToHome() {
       this.$router.push('home');
     },
+    ...mapActions([
+      'setActiveTab',
+    ]),
   },
 };
 </script>
 
 <style lang="scss">
 .app-nav-container {
-  background-color: blue;
+  background: linear-gradient(180deg,#0b2677,#19549b);
   @include flex-row;
-  margin-bottom: 1%;
-  justify-content: space-around;
   align-items: center;
+  font-family: $comic-font-family;
 
   &__logo {
     color: white;
     width: 10%;
     cursor: pointer;
+    margin-right: 3%;
   }
 
-  &__about-us {
-    color: white;
-    text-decoration: none;
-    font-size: $large-font;
-  }
+  &__links {
+    @include flex-row;
+    align-items: center;
+    width: 90%;
 
-  &__coupons {
-    color: white;
-    text-decoration: none;
-    font-size: $large-font;
-  }
+    .router-link-exact-active {
+      opacity: 1;
+    }
 
-  &__businesses {
-    color: white;
-    text-decoration: none;
-    font-size: $large-font;
+    &__home {
+      color: white;
+      text-decoration: none;
+      font-size: $large-font;
+      margin-right: 2%;
+      opacity: 0.5;
+
+      &:hover {
+        opacity: 1;
+      }
+
+      &--active {
+        opacity: 1;
+      }
+
+      .router-link-exact-active {
+        opacity: 1;
+      }
+    }
+
+    &__coupons {
+      color: white;
+      text-decoration: none;
+      font-size: $large-font;
+      margin-right: 2%;
+      opacity: 0.5;
+
+      &:hover {
+        opacity: 1;
+      }
+
+      &--active {
+        opacity: 1;
+      }
+
+      .router-link-exact-active {
+        opacity: 1;
+      }
+    }
+
+    &__businesses {
+      color: white;
+      text-decoration: none;
+      font-size: $large-font;
+      margin-right: 2%;
+      opacity: 0.5;
+
+      &:hover {
+        opacity: 1;
+      }
+
+      &--active {
+        opacity: 1;
+      }
+    }
+
+    &__about-us {
+      color: white;
+      text-decoration: none;
+      font-size: $large-font;
+      margin-right: 2%;
+      opacity: 0.5;
+
+      &:hover {
+        opacity: 1;
+      }
+
+      &--active {
+        opacity: 1;
+      }
+    }
   }
 }
 
