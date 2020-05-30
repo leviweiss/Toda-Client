70c7b5f6e1545f685d829e6f00f9b8a3d1a5de99
Levi Weiss <levi.weiss3@gmail.com>
2020-05-30 17:38:28 +0300

    done with data businesses

--- src/components/home/dataList/DataBusinesses.vue
+++ src/components/home/dataList/DataBusinesses.vue
@@ -1,27 +1,64 @@
 <template>
-  <div id="data-list" class="data-list-container">
+  <div id="data-businesses" class="data-businesses-container">
+    <div class="data-businesses-container__businesses" v-for="business in allBusinessesInfo" :key="business.id"
+    @click="openBusinessModal(business)">
+      <business v-bind="business"/>
+    </div>
+    <business-modal v-show="showBusinessModal" @close="closeBusinessModal"/>
   </div>
 </template>
 
 <script>
-import { mapState, mapActions } from 'vuex';
+
+import {
+  mapState,
+  mapActions,
+  mapGetters,
+} from 'vuex';
+
+import Business from '../../entities/Business.vue';
+import BusinessModal from '../../entities/BusinessModal.vue';
 
 export default {
-  name: 'data-list',
+  name: 'data-coupons',
+  components: {
+    Business,
+    BusinessModal,
+  },
+  beforeMount() {
+    return this.getAllData();
+  },
   computed: {
     ...mapState({
-      chosenCategory: (state) => state.chosenCategory,
-      categories: (state) => state.categories,
+      showBusinessModal: (state) => state.showBusinessModal,
+    }),
+    ...mapGetters({
+      allBusinessesInfo: 'allBusinessesInfo',
     }),
   },
   methods: {
     ...mapActions([
-      'setChosenCategory',
+      'openBusinessModal',
+      'closeBusinessModal',
+      'getAllData',
     ]),
   },
 };
 </script>
 
 <style lang="scss">
+.data-businesses-container {
+  @include flex-row;
+  align-items: center;
+  flex-wrap: wrap;
+  justify-content: space-around;
+
+  &__business {
+    margin-bottom: 3%;
+    width: (100% / 3) - 5%;
+    margin-right: 1%;
+    margin-left: 1%;
+  }
+}
 
 </style>
